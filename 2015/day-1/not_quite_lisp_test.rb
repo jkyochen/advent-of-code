b7646491bcec1c69c1de8776bb2815dd27027251
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-03 08:51:44 +0900

    Add test for NotQuiteLisp

--- 2015/day-1/not_quite_lisp_test.rb
+++ 2015/day-1/not_quite_lisp_test.rb
@@ -3,20 +3,26 @@ require_relative 'not_quite_lisp'
 
 class NotQuiteLispTest < Minitest::Test
 
-    def test_empty
+    def test_0
         assert_equal 0, NotQuiteLisp.take_santa_to_what_floor("")
+        assert_equal 0, NotQuiteLisp.take_santa_to_what_floor("(())")
+        assert_equal 0, NotQuiteLisp.take_santa_to_what_floor("()()")
     end
 
-    def test_basic
-        assert_equal 0, NotQuiteLisp.take_santa_to_what_floor("()")
-    end
-
-    def test_up_floor
+    def test_up_floor_3
         assert_equal 3, NotQuiteLisp.take_santa_to_what_floor("(((")
+        assert_equal 3, NotQuiteLisp.take_santa_to_what_floor("(()(()(")
+        assert_equal 3, NotQuiteLisp.take_santa_to_what_floor("))(((((")
     end
 
-    def test_down_floor
+    def test_down_floor_1
+        assert_equal -1, NotQuiteLisp.take_santa_to_what_floor("())")
+        assert_equal -1, NotQuiteLisp.take_santa_to_what_floor("))(")
+    end
+
+    def test_down_floor_3
         assert_equal -3, NotQuiteLisp.take_santa_to_what_floor(")))")
+        assert_equal -3, NotQuiteLisp.take_santa_to_what_floor(")())())")
     end
 
 end
