d1e693919fb8603a881d419c14b34c4223e14f5a
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-05 17:40:14 +0900

    Refactor operate direction to move method

--- 2015/day-3/house.rb
+++ 2015/day-3/house.rb
@@ -7,25 +7,29 @@ class House
         current_location = init_location.dup
         move_directions.chars.each do |direction|
             current_location = current_location.dup
-            if direction == "^"
-                # north
-                current_location[1] += 1
-            elsif direction == "v"
-                # south
-                current_location[1] -= 1
-            elsif direction == ">"
-                # east
-                current_location[0] += 1
-            elsif direction == "<"
-                # west
-                current_location[0] -= 1
-            end
+            move(direction, current_location)
             push_diff_location(receive_present_house, current_location)
         end
         receive_present_house.length
     end
 
     private
+
+    def self.move(direction, current_location)
+        if direction == "^"
+            # north
+            current_location[1] += 1
+        elsif direction == "v"
+            # south
+            current_location[1] -= 1
+        elsif direction == ">"
+            # east
+            current_location[0] += 1
+        elsif direction == "<"
+            # west
+            current_location[0] -= 1
+        end
+    end
 
     def self.push_diff_location(receive_present_house, current_location)
         return if receive_present_house.any? { |lo| lo == current_location }
