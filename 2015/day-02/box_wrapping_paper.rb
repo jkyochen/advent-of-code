ff5dd5a7a39ace8a7af89431e2e1e1d04ac1fb89
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-13 18:07:41 +0900

    Update day 2 puzzle input test

--- 2015/day-02/box_wrapping_paper.rb
+++ 2015/day-02/box_wrapping_paper.rb
@@ -1,6 +1,17 @@
 
 class BoxWrappingPaper
 
+    def self.cals(instr)
+        result = 0
+        instr.split("\n").each do |i|
+            l, w, h = i.split("x")
+            result += cal(l.to_i, w.to_i, h.to_i)
+        end
+        result
+    end
+
+    private
+
     def self.cal(l, w, h)
         m1, m2 = [l, w, h].min(2)
         2*(l*w + l*h + w*h) + m1*m2
--- 2015/day-02/box_wrapping_paper_test.rb
+++ 2015/day-02/box_wrapping_paper_test.rb
@@ -4,11 +4,23 @@ require_relative 'box_wrapping_paper.rb'
 class BoxWrappingPaperTest < Minitest::Test
 
     def test_1
-        assert_equal 58, BoxWrappingPaper.cal(2, 3, 4)
+        assert_equal 58, BoxWrappingPaper.cals("2x3x4")
     end
 
     def test_2
-        assert_equal 43, BoxWrappingPaper.cal(1, 1, 10)
+        assert_equal 43, BoxWrappingPaper.cals("1x1x10")
+    end
+
+    def test_3
+        assert_equal 58+43, BoxWrappingPaper.cals("
+            2x3x4
+            1x1x10")
+    end
+
+    @@puzzle_input = File.read("./puzzle_input.txt")
+
+    def test_puzzle_input
+        assert_equal 1586300, BoxWrappingPaper.cals(@@puzzle_input)
     end
 
 end
