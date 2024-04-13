dd27538083f1aecad6cfa0be578ae6770536f767
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-13 21:43:15 +0900

    Add day 3 part 2

--- 2015/day-03/house.rb
+++ 2015/day-03/house.rb
@@ -14,6 +14,24 @@ class House
         receive_present_house.length
     end
 
+    def self.count_receive_present_house_with_robot(move_directions)
+        receive_present_house = [START_LOCATION]
+        current_location = START_LOCATION.dup
+        robot_current_location = START_LOCATION.dup
+        move_directions.chars.each_with_index do |direction, i|
+            if i%2 == 0
+                current_location = current_location.dup
+                move(direction, current_location)
+                push_diff_location(receive_present_house, current_location)
+            else
+                robot_current_location = robot_current_location.dup
+                move(direction, robot_current_location)
+                push_diff_location(receive_present_house, robot_current_location)
+            end
+        end
+        receive_present_house.length
+    end
+
     private
 
     def self.move(direction, current_location)
--- 2015/day-03/house_test.rb
+++ 2015/day-03/house_test.rb
@@ -15,7 +15,21 @@ class HouseTest < Minitest::Test
         assert_equal 2, House.count_receive_present_house("^v^v^v^v^v")
     end
 
+    def test_basic_with_robot
+        assert_equal 3, House.count_receive_present_house_with_robot("^v")
+    end
+
+    def test_basic_with_robot2
+        assert_equal 3, House.count_receive_present_house_with_robot("^>v<")
+    end
+
+    def test_basic_with_robot3
+        assert_equal 11, House.count_receive_present_house_with_robot("^v^v^v^v^v")
+    end
+
     def test_puzzle_input
-        assert_equal 2565, House.count_receive_present_house(File.read("puzzle_input.txt"))
+        puzzle_input = File.read("puzzle_input.txt")
+        assert_equal 2565, House.count_receive_present_house(puzzle_input)
+        assert_equal 2639, House.count_receive_present_house_with_robot(puzzle_input)
     end
 end
