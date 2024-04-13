3215b7a853a237871f83f4e0dad652cb3de17dc7
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-13 18:34:06 +0900

    Add day 2 part 2

--- 2015/day-02/box_wrap.rb
+++ 2015/day-02/box_wrap.rb
@@ -10,7 +10,21 @@ class BoxWrap
         result
     end
 
+    def self.feets_with_ribbon(instr)
+        result = 0
+        instr.split("\n").each do |i|
+            l, w, h = i.split("x")
+            result += feet_with_ribbon(l.to_i, w.to_i, h.to_i)
+        end
+        result
+    end
+
     private
+
+    def self.feet_with_ribbon(l, w, h)
+        m1, m2 = [l, w, h].min(2)
+        l*w*h + 2*(m1+m2)
+    end
 
     def self.feet(l, w, h)
         m1, m2 = [l, w, h].min(2)
--- 2015/day-02/box_wrap_test.rb
+++ 2015/day-02/box_wrap_test.rb
@@ -17,10 +17,24 @@ class BoxWrapTest < Minitest::Test
             1x1x10")
     end
 
-    @@puzzle_input = File.read("./puzzle_input.txt")
+    def test_with_ribbon1
+        assert_equal 34, BoxWrap.feets_with_ribbon("2x3x4")
+    end
+
+    def test_with_ribbon2
+        assert_equal 14, BoxWrap.feets_with_ribbon("1x1x10")
+    end
+
+    def test_with_ribbon3
+        assert_equal 34+14, BoxWrap.feets_with_ribbon("
+            2x3x4
+            1x1x10")
+    end
 
     def test_puzzle_input
-        assert_equal 1586300, BoxWrap.feets(@@puzzle_input)
+        puzzle_input = File.read("./puzzle_input.txt")
+        assert_equal 1586300, BoxWrap.feets(puzzle_input)
+        assert_equal 3737498, BoxWrap.feets_with_ribbon(puzzle_input)
     end
 
 end
