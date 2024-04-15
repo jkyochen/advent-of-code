59deb9cd1caab8bd38b25a988d06416419c3f3fb
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-15 18:16:25 +0900

    Add day 5 part 2

--- 2015/day-05/text.rb
+++ 2015/day-05/text.rb
@@ -7,7 +7,33 @@ class Text
         end
     end
 
+    def self.count_nice_part2(inputs)
+        inputs.split("\n").count do |i|
+            nice_part2?(i)
+        end
+    end
+
     private
+
+    def self.nice_part2?(strings)
+        return false if strings.length < 4
+
+        # It contains a pair of any two letters that appears at least twice in the string without overlapping.
+        isRepeatLetter = false
+        for i in 0...strings.length-2
+            if strings.slice(i+2..).include?(strings[i]+strings[i+1])
+                isRepeatLetter = true
+                break
+            end
+        end
+        return false if !isRepeatLetter
+
+        # It contains at least one letter which repeats with exactly one letter between them
+        for i in 0...strings.length-2
+            return true if strings[i] == strings[i+2]
+        end
+        false
+    end
 
     def self.nice?(strings)
         # 1. at least three vowels (aeiou only)
--- 2015/day-05/text_test.rb
+++ 2015/day-05/text_test.rb
@@ -23,8 +23,29 @@ class TextTest < Minitest::Test
         assert_equal 0, Text.count_nice("dvszwmarrgswjxmb")
     end
 
+    def test_part2_1
+        assert_equal 1, Text.count_nice_part2("qjhvhtzxzqqjkmpb")
+    end
+
+    def test_part2_1
+        assert_equal 1, Text.count_nice_part2("qjhvhtzxzqqjkmpb")
+    end
+
+    def test_part2_2
+        assert_equal 1, Text.count_nice_part2("xxyxx")
+    end
+
+    def test_part2_3
+        assert_equal 0, Text.count_nice_part2("uurcxstgmygtbstg")
+    end
+
+    def test_part2_4
+        assert_equal 0, Text.count_nice_part2("ieodomkazucvgmuy")
+    end
+
     def test_puzzle_input
         puzzle_input = File.read("puzzle_input.txt")
         assert_equal 238, Text.count_nice(puzzle_input)
+        assert_equal 69, Text.count_nice_part2(puzzle_input)
     end
 end
