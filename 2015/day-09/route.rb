003ee453ec2ace5884802c549842067d6ad45553
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-18 12:31:34 +0900

    Refactor change city_obj to object variable and move set logic

--- 2015/day-09/route.rb
+++ 2015/day-09/route.rb
@@ -21,30 +21,33 @@ class Route
         @citys = Set[] if !@citys
         @list_distance = [] if !@list_distance
 
-        city_obj = {}
+        @city_obj = {}
         data.split("\n").each do |str|
             match_data = str.match(@@route_regex)
             next if !match_data
-
-            from_city = match_data[1]
-            to_city = match_data[2]
-            distance = match_data[3].to_i
-            @citys << from_city
-            @citys << to_city
-
-            city_obj[from_city] = {} if !city_obj[from_city]
-            city_obj[to_city] = {} if !city_obj[to_city]
-
-            city_obj[from_city][to_city] = distance
-            city_obj[to_city][from_city] = distance
+            save_city(match_data)
         end
 
-        city_obj.each_pair do |from_city, routes|
-            pick_to_city(city_obj, city_obj[from_city], [from_city], 0)
+        @city_obj.each_pair do |from_city, routes|
+            pick_to_city(@city_obj[from_city], [from_city], 0)
         end
     end
 
-    def pick_to_city(city_obj, routes, pick_citys, pick_distance)
+    def save_city(match_data)
+        from_city = match_data[1]
+        to_city = match_data[2]
+        distance = match_data[3].to_i
+        @citys << from_city
+        @citys << to_city
+
+        @city_obj[from_city] = {} if !@city_obj[from_city]
+        @city_obj[to_city] = {} if !@city_obj[to_city]
+
+        @city_obj[from_city][to_city] = distance
+        @city_obj[to_city][from_city] = distance
+    end
+
+    def pick_to_city(routes, pick_citys, pick_distance)
         return if !routes
 
         if pick_citys.length == @citys.length
@@ -58,7 +61,7 @@ class Route
             pick_citys << city
             pick_distance += distance
 
-            pick_to_city(city_obj, city_obj[city], pick_citys, pick_distance)
+            pick_to_city(@city_obj[city], pick_citys, pick_distance)
 
             pick_distance -= distance
             pick_citys.pop
