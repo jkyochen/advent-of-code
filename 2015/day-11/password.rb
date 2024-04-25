19eaa6ffd4357b1ff069d210bc2cc74929715034
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-25 14:23:57 +0900

    Update skip mistake letter

--- 2015/day-11/password.rb
+++ 2015/day-11/password.rb
@@ -4,22 +4,29 @@ class Password
     def self.next(old)
         loop do
             old = old.succ
+            old = skip_mis_letter(old)
             return old if check_pass(old)
         end
     end
 
     private
 
+    def self.skip_mis_letter(old)
+        if old.count('iol') > 0
+            letter_index = mis_letter_index(old)
+            old = old[0..letter_index].succ + 'a'*(old.length-(letter_index+1))
+        end
+        old
+    end
+
+    def self.mis_letter_index(picks)
+        picks.index("i") || picks.index("o") || picks.index("l")
+    end
+
     def self.check_pass(picks)
-        return false if check_mistaken_letter(picks)
         return false if !check_inc_three_letter(picks)
         return false if !check_two_repeat_pair_letter(picks)
         true
-    end
-
-    def self.check_mistaken_letter(picks)
-        return true if picks.count('iol') > 0
-        false
     end
 
     def self.check_two_repeat_pair_letter(picks)
