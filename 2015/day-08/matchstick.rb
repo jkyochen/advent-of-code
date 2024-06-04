e5e885803e1397a4a8462524bc499759f261775a
ChenZiQiang <ziqiang.chen@outlook.com>
2024-06-05 00:02:34 +0900

    Refactor rename variable name

--- 2015/day-08/matchstick.rb
+++ 2015/day-08/matchstick.rb
@@ -1,14 +1,14 @@
 
 class Matchstick
 
-    def self.diff_counts(strings)
-        strings.split("\n").sum do |str|
+    def self.diff_counts(strs)
+        strs.split("\n").sum do |str|
             str.length - str.undump.length
         end
     end
 
-    def self.diff_counts_part2(strings)
-        strings.split("\n").sum do |str|
+    def self.diff_counts_part2(strs)
+        strs.split("\n").sum do |str|
             str.dump.length - str.length
         end
     end
