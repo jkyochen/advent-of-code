d4f0bec078df783e4359584d480c74b0e0918172
ChenZiQiang <ziqiang.chen@outlook.com>
2024-06-04 23:57:17 +0900

    Refactor move loop to method

--- 2015/day-10/look_and_say.rb
+++ 2015/day-10/look_and_say.rb
@@ -4,20 +4,26 @@ class LookAndSay
     def self.say(value, times = 1)
         value = value.to_s
         times.times do
-            temp = ""
-            i = 0
-            for j in 0...value.length
-                if value[j] != value[i]
-                    temp += (j-i).to_s + value[i]
-                    i = j
-                end
-                if j == value.length-1
-                    temp += (j-i+1).to_s + value[i]
-                end
-            end
-            value = temp
+            value = loop_to_say(value)
         end
         value.to_i
+    end
+
+    private
+
+    def self.loop_to_say(value)
+        temp = ""
+        i = 0
+        for j in 0...value.length
+            if value[j] != value[i]
+                temp += (j-i).to_s + value[i]
+                i = j
+            end
+            if j == value.length-1
+                temp += (j-i+1).to_s + value[i]
+            end
+        end
+        temp
     end
 
 end
