c48e7e5d113d21766783a25de5f14f992cd82977
ChenZiQiang <ziqiang.chen@outlook.com>
2024-06-22 23:17:37 +0900

    Add olympics part2

--- 2015/day-14/olympics.rb
+++ 2015/day-14/olympics.rb
@@ -8,6 +8,7 @@ class Olympics
     end
 
     def win_km_in_time
+        return 0 if @players.empty?
         max_traveled = 0
         @players.each do |player|
             distance = cal_distance(@limt_time, player)
@@ -16,12 +17,50 @@ class Olympics
         max_traveled
     end
 
+    def win_point_in_time
+        return 0 if @players.empty? || @limt_time.zero?
+        return @limt_time if @players == 1
+
+        h = Hash.new(0)
+        @limt_time.times do |i|
+            time = i+1
+            all_player_status = get_player_status_in_time(time)
+            max_distance = get_max_distance(all_player_status)
+
+            all_player_status.each do |p|
+                next if p[:distance] != max_distance
+                h[p[:name]]+=1
+            end
+        end
+        h.values.max
+    end
+
     private
+
+    def get_max_distance(all_player_status)
+        max_distance = 0
+        all_player_status.each do |p|
+            if p[:distance] > max_distance
+                max_distance = p[:distance]
+            end
+        end
+        max_distance
+    end
+
+    def get_player_status_in_time(time)
+        @players.map do |player|
+            distance = cal_distance(time, player)
+            {
+                name: player[:name],
+                distance: distance,
+            }
+        end
+    end
 
     def get_player(info)
         md = REINDEER_RE.match(info)
         {
-            name: md[1].to_i,
+            name: md[1],
             speed: md[2].to_i,
             run_time: md[3].to_i,
             sleep_time: md[4].to_i,
@@ -29,7 +68,7 @@ class Olympics
     end
 
     def cal_distance(time, p)
-        q, r = @limt_time.divmod(p[:run_time] + p[:sleep_time])
+        q, r = time.divmod(p[:run_time] + p[:sleep_time])
         if r > p[:run_time]
             (q+1)*p[:run_time] * p[:speed]
         else
--- 2015/day-14/olympics_test.rb
+++ 2015/day-14/olympics_test.rb
@@ -7,11 +7,24 @@ class OlympicsTest < Minitest::Test
         Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
         Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
         END
-        assert_equal 1120, Olympics.new(reindeers_info, 1000).win_km_in_time()
+        assert_equal 1120, Olympics.new(reindeers_info, 1000).win_km_in_time
     end
 
     def test_puzzle_input
         puzzle_input = File.read("puzzle_input.txt")
-        assert_equal 2660, Olympics.new(puzzle_input, 2503).win_km_in_time()
+        assert_equal 2660, Olympics.new(puzzle_input, 2503).win_km_in_time
+    end
+
+    def test_basic_part2
+        reindeers_info = <<~'END'
+        Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+        Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+        END
+        assert_equal 689, Olympics.new(reindeers_info, 1000).win_point_in_time
+    end
+
+    def test_puzzle_input_part2
+        puzzle_input = File.read("puzzle_input.txt")
+        assert_equal 0, Olympics.new(puzzle_input, 2503).win_point_in_time
     end
 end
