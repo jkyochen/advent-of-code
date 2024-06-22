6647627a03e0ae728962d6f15bbbac5c7a45ae6d
ChenZiQiang <ziqiang.chen@outlook.com>
2024-06-22 19:25:13 +0900

    Add parse sentence

--- 2015/day-14/olympics.rb
+++ 2015/day-14/olympics.rb
@@ -1,29 +1,37 @@
 class Olympics
 
+    REINDEER_RE = /(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./
+
     def initialize(reindeers_info)
         @reindeers_info = reindeers_info
     end
 
-    def win_km_when_time(time, players)
+    def win_km_in_time(time)
         max = 0
-        players.each do |player|
-            distance = cal_distance(time, player)
-            if distance > max
-                max = distance
-            end
+        @reindeers_info.split("\n").each do |info|
+            distance = cal_distance(time, get_player(info))
+            max = [distance, max].max
         end
         max
     end
 
     private
 
-    def cal_distance(time, player)
-        speed, run_time, sleep_time = player
-        q, r = time.divmod(run_time + sleep_time)
-        if r > run_time
-            (q + 1) * run_time * speed
+    def get_player(info)
+        md = REINDEER_RE.match(info)
+        {
+            speed: md[2].to_i,
+            run_time: md[3].to_i,
+            sleep_time: md[4].to_i,
+        }
+    end
+
+    def cal_distance(time, p)
+        q, r = time.divmod(p[:run_time] + p[:sleep_time])
+        if r > p[:run_time]
+            (q+1)*p[:run_time] * p[:speed]
         else
-            q * run_time * speed + r * speed
+            (q*p[:run_time]+r) * p[:speed]
         end
     end
 end
--- 2015/day-14/olympics_test.rb
+++ 2015/day-14/olympics_test.rb
@@ -7,6 +7,11 @@ class OlympicsTest < Minitest::Test
         Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
         Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
         END
-        assert_equal 1120, Olympics.new(reindeers_info).win_km_when_time(1000, [[14, 10, 127], [16, 11, 162]])
+        assert_equal 1120, Olympics.new(reindeers_info).win_km_in_time(1000)
+    end
+
+    def test_puzzle_input
+        puzzle_input = File.read("puzzle_input.txt")
+        assert_equal 0, Olympics.new(puzzle_input).win_km_in_time(2503)
     end
 end
